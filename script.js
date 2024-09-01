3454741b0fb626e3577833eda282155f16e39a57
rs-drawz <sabarishr.21it@kongu.edu>
2024-09-01 10:55:38 +0530

    initial commit
    query bot for my resume

---
+++ script.js
@@ -0,0 +1,236 @@
+var audio = new Audio('assets/sentmessage.mp3');
+var contactString = `<div class='social'> 
+                        <a target='_blank' href='tel:+918438678326'>
+                         <div class='socialItem' id='call'>
+                         <img class='socialItemI' src='images/phone.svg'/>
+                         <label class='number'>8438678326</label></div> </a>
+                          <a href='mailto:sabarishr.21it@kongu.edu'> 
+                            <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div>
+                          </a> 
+                          <a target='_blank' href='https://github.com/rs-drawz'> 
+                                <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> 
+                          </a> 
+                          <a target='_blank' href='https://www.instagram.com/rs_drawz/'> <div class='socialItem'>
+                            <img class='socialItemI' src='images/instagram.svg' alt=''> </div>
+                          </a> 
+                          <a href='https://www.linkedin.com/in/rsdrawz/' target='_blank' rel='noopener noreferrer'>
+                            <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div>
+                          </a> 
+                        </div>`;
+var resumeString = `<img src='images/resumeThumbnail.png' class='resumeThumbnail'>
+                        <div class='downloadSpace'><div class='pdfname'>
+                            <img src='images/pdf.png'>
+                                <label>Vinayak Resume.pdf</label>
+                                </div><a href='assets/Vinayak Patil&#39;s Resume.pdf' download='Vinayak_Patil_Resume.pdf'>
+                                <img class='download' src='images/downloadIcon.svg'></a></div>`;
+var addressString = `<div class='mapview'>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3912.826182806128!2d77.6044578!3d11.2741843!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba96d7810fe32d5%3A0x85cf49c5b26fb72e!2sKongu%20Engineering%20College!5e0!3m2!1sen!2sin!4v1725162840063!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                    <label class='add'>
+                        <address>
+                            KEC<br>
+                            Perundurai, Erode, Tamil Nadu, INDIA 638060
+                        </address>
+                    </label`
+function startFunction() {
+    setLastSeen();
+    waitAndResponce("intro");
+}
+
+function setLastSeen() {
+    var date = new Date();
+    var lastSeen = document.getElementById("lastseen");
+    lastSeen.innerText = "last seen today at " + date.getHours() + ":" + date.getMinutes()
+}
+
+
+function closeFullDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
+function openFullScreenDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
+
+function isEnter(event) {
+    if (event.keyCode == 13) {
+        sendMsg();
+    }
+}
+
+function sendMsg() {
+    var input = document.getElementById("inputMSG");
+    var ti = input.value;
+    if (input.value == "") {
+        return;
+    }
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "sent");
+    greendiv.setAttribute("class", "green");
+    dateLabel.setAttribute("class", "dateLabel");
+    greendiv.innerText = input.value;
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    setTimeout(function () { waitAndResponce(ti) }, 1500);
+    input.value = "";
+    playSound();
+}
+
+function waitAndResponce(inputText) {
+    var lastSeen = document.getElementById("lastseen");
+    lastSeen.innerText = "typing...";
+    switch (inputText.toLowerCase().trim()) {
+        case "intro":
+            setTimeout(() => {
+                sendTextMessage(`Hello there 👋🏻,<br><br>My name is <span class='bold'>
+                    <a class='alink'>Sabarish Ramesh</a>.
+                    </span><br><br>I am a IT student at <span class='bold'>KEC👨🏻‍💻📚</span>
+                    <br><br>Send <span class='bold'>'help'</span> to know more about me.<br>`);
+            }, 2000);
+            break;
+        case "help":
+            sendTextMessage(`<span class='sk'>
+                Send Keyword to get what you want to know about me...<br>
+                e.g<br><span class='bold'>'skills'</span> - to know my skills<br>
+                <span class='bold'>'resume'</span> - to get my resume<br>
+                <span class='bold'>'education'</span> - to get my education details<br><span class='bold'>'address'</span> - to get my address<br><span class='bold'>
+                'contact'</span> - to get ways to connect with me<br><span class='bold'>'projects'</span> - to get details of my projects<br>
+                <span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span>`);
+            break;
+        case "resume":
+            sendTextMessage(resumeString);
+            break;
+        case "skills":
+            sendTextMessage(`<span class='sk'>
+                I am currently pursuing B.Tech degree in Information Technology.<br>
+                <br>I can comfortably write code in following languages :<br>
+                <span class='bold'>Java<br>JavaScript<br>CSS<br>HTML</span><br>
+                <br>I know the following frameworks :<span class='bold'>
+                <br>React.js<br>Express.js</span><br>
+                <br>I know the following runtime environment:<span class='bold'>
+                <br>Node.js</span><br>
+                <br>I know the following Database:<span class='bold'>
+                <br>MongoDB</span><br>
+                <br>I am learning<span class='bold'>
+                <br>Vue.js <br>AWS</span><br>
+                <br>Favourite IDE:Visual Studio Code(VScode)</span>`);
+            break;
+
+        case "education":
+            sendTextMessage(`<span class='sk'>
+                    <span class='bold'>
+                    B.Tech in Information Technology Engineering
+                    </span><br>
+                    Kongu Engineering College (KEC)
+                    Expected Graduation: 2025<br>
+                <br>
+                    <span class='bold'>
+                    Secondary School <br>
+                    </span>
+                    Cheran Matric Hr Sec School Graduated: 2021
+                    </span>`);
+            break;
+
+        case "address":
+            sendTextMessage(addressString);
+            break;
+        case "clear":
+            clearChat();
+            break;
+        case "about":
+            sendTextMessage("🛠️💻 Here is my portfolio!<br><br>👨🏻‍💻 Designed and Developed by <a class='alink' target='_blank' href=''https://rs-drawz.github.io/Sabarish_Portfolio''><span class='bold'>Sabarish Ramesh</a> with ❤️</span>");
+            break;
+        case "contact":
+            sendTextMessage(contactString);
+            break;
+        case "projects":
+            sendTextMessage("You want to check my projects? Then just jump into my Github Account.<br><br><div class='social'><a target='_blank' href='https://github.com/rs-drawz'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a></div>");
+            break;
+        case "new":
+            sendTextMessage(addressString);
+            break;
+        default:
+            setTimeout(() => {
+                sendTextMessage("Hey I couldn't catch you...😢<br>Send 'help' to know more about usage.<br>Make sure you entered the query correctly");
+            }, 2000);
+            break;
+    }
+
+
+
+}
+
+function clearChat() {
+    document.getElementById("listUL").innerHTML = "";
+    waitAndResponce('intro');
+}
+
+
+
+function sendTextMessage(textToSend) {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.setAttribute("id", "sentlabel");
+    dateLabel.id = "sentlabel";
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
+    greendiv.innerHTML = textToSend;
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
+}
+
+
+function sendResponse() {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
+    dateLabel.setAttribute("class", "dateLabel");
+    greendiv.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ";
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
+}
+
+function playSound() {
+    audio.play();
+}
